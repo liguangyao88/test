7d760f65d26dfedd565fe74f131644f2ca5ac138
wangdashenzaixian <2249169092@qq.com>
2017-04-25 15:52:24 +0800

    4.25_15:52_gao

--- src/script/components/mine/children/MineLogin.js
+++ src/script/components/mine/children/MineLogin.js
@@ -5,12 +5,18 @@ export default class MineLogin extends Component {
 		return (
 			<div className="mine_login">
 			          <div className="mine_head">11</div>
-			          <h2>注册登录</h2>
-			          
+			          <h2>注册登录</h2>			          
 			          <form>
-			              <input       />
-			          
-			          
+			              <div>
+			                    <i></i>
+			              		<input type="text" name="user" placeholder="输入手机号"  className="user" />
+			              </div>
+			              <div>
+			                     <i></i>
+			                     <input type="password" name="pass" placeholder="输入密码"  className="pass"/>
+			                     <b></b>
+			                </div>
+			               <input type="submit"  value="确定"/>
 			          </form>
 			          
 			</div>
